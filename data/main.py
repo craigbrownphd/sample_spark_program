f188c3b21bfd992e926321f5995291b4b911f30f
Wen-Ting Zhu <wz6hd@virginia.edu>
2016-05-01 22:41:02 -0400

    o

--- data/main.py
+++ data/main.py
@@ -10,7 +10,7 @@ sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
 # 0. Read data in
 # each worker loads a piece of the data file
-data = sc.textFile("/tmp/data/access.log", 2)
+data = sc.textFile("/tmp/data/access3.log", 2)
 
 # 1. line -> (user_id, item_id clicked on by the user)
 step1 = data.map(lambda line: line.split(" "))
@@ -21,19 +21,29 @@ step2 = step1.groupByKey()
 # 3. Transform into (user_id, (item1, item2)) where item1 and item2 are pairs of items the user clicked on
 # NOTE: if a user only liked 1 item, then that user's items will not be considered.
 def combine(s2):
-    arr = list(s2[1])
+    # remove duplicates
+    arr = list(set(s2[1]))
     out = []
-    for combination in itertools.combinations(arr, 2):
-        out.append((s2[0], combination))
+    for i in range(0, len(arr)):
+        for j in range(i+1, len(arr)):
+            out.append((s2[0], (arr[i], arr[j])))
+    # for combination in itertools.combinations(arr, 2):
+    #     out.append((s2[0], combination))
     return out
 step3 = step2.flatMap(combine)
+# print_rdd(step3, "step3: (user_id, (item1, item2))")
 
 # 4. Transform into ((item1, item2), list of user1, user2 etc) where users are all the ones who co-clicked (item1, item2)
 def reverse(s3):
     return (s3[1], s3[0])
 step4 = step3.map(reverse).groupByKey()
+# print_rdd(step4, "step4: ((item1, item2), [user])")
 
 # 5. Transform into ((item1, item2), count of distinct users who co-clicked (item1, item2)
+# step51 = step4.map(reverse).distinct()
+# printrdd_rdd(step51, "")
+# step52 = step51.map(reverse)
+# NOTE: need to sort the pair
 step5 = step4.map(lambda s4: (s4[0], len(s4[1])))
 # print_rdd(step5, "step5: ((item1, item2), len([user_id]))")
 
@@ -41,9 +51,9 @@ step5 = step4.map(lambda s4: (s4[0], len(s4[1])))
 step6 = step5.filter(lambda s5: s5[1]>2)
 # print_rdd(step6, "step6 (filtered): ((item1, item2), len([user_id]))")
 
-# output results
+# Output results
 for t in step6.collect():
-    print("OUTPUT: Items {} and {} were co-clicked {} times.".format(t[0][0], t[0][1], t[1]))
+    print("\nOUTPUT: Items {} and {} were co-clicked {} times.\n".format(t[0][0], t[0][1], t[1]))
     # step6.saveAsTextFile("/tmp/data/results.output")
 
 sc.stop()
