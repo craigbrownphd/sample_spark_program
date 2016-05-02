038f9afa692bbdf2a73a38bf85d623360001af69
himanshu <himanshuo@gmail.com>
2016-05-01 21:02:26 -0400

    create test data

---
+++ create_test_data.py
@@ -0,0 +1,24 @@
+# sample input file
+# u0 i0
+# u0 i1
+# u0 i3
+# u1 i2
+# u1 i3
+# u2 i1
+# u2 i2
+# u2 i3
+# u3 i0
+# u3 i1
+# u4 i0
+# u4 i1
+# u4 i2
+# u4 i3
+FILEPATH = "./data/access.log"
+NUM_USERS = 10
+NUM_ITEMS = 100
+NUM_COCLICKED_ITEMS = 10
+with open(FILEPATH, 'w') as f:
+    for u in range(0,NUM_USERS):
+        for i in range(0,NUM_ITEMS):
+            if u < i:
+                f.write("user_{} item_{}\n".format(u,i))
